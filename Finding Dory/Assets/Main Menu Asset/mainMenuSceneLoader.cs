e9f457086f16ddda7be69284142b3a0a1d1e6bac
Marvin <mjae0109@gmail.com>
2023-01-07 19:53:05 +0800

    Main Menu Added

--- Finding Dory/Assets/Main Menu Asset/mainMenuSceneLoader.cs
+++ Finding Dory/Assets/Main Menu Asset/mainMenuSceneLoader.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class mainMenuSceneLoader : MonoBehaviour
 {
-    public void nextScene()
+    public void selectionScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Game Closed");
+        Application.Quit();
     }
 }
