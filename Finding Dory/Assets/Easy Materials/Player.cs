b3cb28fba3886b411f136c680255fedc0ed33024
Christian Carig Clavecillas <clamumuu@gmail.com>
2023-01-18 18:41:53 +0800

    Easy difficulty added

---
+++ Finding Dory/Assets/Easy Materials/Player.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 direction;
+
+    public float gravity = -9.8f;
+
+    public float strength = 5f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            direction = Vector3.up * strength;
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            FindObjectOfType<GameManager>().GameOver();
+        }
+        else if (other.gameObject.CompareTag("Scoring"))
+        {
+            FindObjectOfType<GameManager>().IncreaseScore();
+        }
+    }
+
+}
