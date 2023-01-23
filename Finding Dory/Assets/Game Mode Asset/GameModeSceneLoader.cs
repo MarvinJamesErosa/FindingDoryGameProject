84c47e277edfaa650c81a9ed3003db742d95cb07
Marvin <mjae0109@gmail.com>
2023-01-23 20:35:01 +0800

    loading screen fixed error fixed in previous commit

--- Finding Dory/Assets/Game Mode Asset/GameModeSceneLoader.cs
+++ Finding Dory/Assets/Game Mode Asset/GameModeSceneLoader.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameModeSceneLoader : MonoBehaviour
 {
-    public void loadingScene()
+    [SerializeField] private GameObject LoadingScreen;
+    [SerializeField] private GameObject GameModeScreen;
+    [SerializeField] private Slider ProgressBar;
+
+    public void GameMode(int BuildIndex)
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        GameModeScreen.SetActive(false);
+        LoadingScreen.SetActive(true);
+
+        StartCoroutine(LoadSequence(BuildIndex));
+    }
+
+    IEnumerator LoadSequence(int BuildIndex)
+    {
+        AsyncOperation Progress = SceneManager.LoadSceneAsync(BuildIndex);
+
+        while (!Progress.isDone)
+        {
+            float ProgressValue = Mathf.Clamp01(Progress.progress / 0.9f);
+            yield return null;
+        }
     }
 }
