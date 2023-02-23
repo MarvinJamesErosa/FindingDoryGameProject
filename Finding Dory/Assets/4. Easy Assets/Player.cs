f66b49ee6921a6c5fe90bd980d63053637af216d
Christian Carig Clavecillas <clamumuu@gmail.com>
2023-02-24 01:42:20 +0800

    bgm adjusted and sfx added

--- Finding Dory/Assets/4. Easy Assets/Player.cs
+++ Finding Dory/Assets/4. Easy Assets/Player.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Vector3 direction;
-
     public float gravity = -9.8f;
-
     public float strength = 5f;
+
+    [SerializeField] private AudioSource clickSoundEffect;
+    [SerializeField] private AudioSource scoreSoundEffect;
+    [SerializeField] private AudioSource gameOverSoundEffect;
+
     private void OnEnable()
     {
         Vector3 position = transform.position;
@@ -18,7 +21,8 @@ public class Player : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-        {
+        {  
+            clickSoundEffect.Play();
             direction = Vector3.up * strength * 3;
         }
 
@@ -29,11 +33,13 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Obstacle"))
-        {
+        {  
+            gameOverSoundEffect.Play();
             FindObjectOfType<GameManager>().Continue();
         }
         else if (other.gameObject.CompareTag("Scoring"))
         {
+            scoreSoundEffect.Play();
             FindObjectOfType<GameManager>().IncreaseScore();
         }
     }
